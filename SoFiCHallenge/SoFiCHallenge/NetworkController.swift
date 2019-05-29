30d93f49fdb333d193863ceca957ec5f77e7606e
David Miller <dmillerconsulting@me.com>
2019-05-29 12:02:14 -0600

    Initial Commit, network controller

---
+++ SoFiCHallenge/SoFiCHallenge/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  SoFiCHallenge
+//
+//  Created by Work on 5/29/19.
+//  Copyright © 2019 Giglioroninomicon. All rights reserved.
+//
+
+import Foundation
---
+++ SoFiCHallenge/SoFiCHallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoFiCHallenge
+//
+//  Created by Work on 5/29/19.
+//  Copyright © 2019 Giglioroninomicon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
